42eede8bbcdc6a3fff1e2cf4a9d6d2750f15487e
tomnico02 <53854225+tomnico02@users.noreply.github.com>
2019-08-07 15:53:27 +0100

    meetingRoom
    07/08/2019

--- meetingRoom.py
+++ meetingRoom.py
@@ -15,45 +15,29 @@ bookingMonth = input("Enter the month you are booking: ")
 bookingYear = int(input("Enter the year you are booking: "))
 
 
-      
 monthsOfYear = {
     
-    'January':01,
-    "February": "02",
-    "March": "03",
-    "April": "04",
-    "May": "05",
-    "June": "06",
-    "July": "07",
-    "August": "08",
-    "September": "09",
-    "October": "10",
-    "November": "11",
-    "December": "12"
+    "January":1,
+    "February":2,
+    "March":3,
+    "April":4,
+    "May":5,
+    "June":6,
+    "July":7,
+    "August":8,
+    "September":9,
+    "October":10,
+    "November":11,
+    "December":12
 }
-    
-if bookingMonth in monthsOfYear.values():
-    bookingMonth=value
-else:
-    print("You have incorrectly entered the month you are booking.")
-
-print(bookingMonth)
 
 
-print("Now you will enter the booking time, please use the following format - 12:30 with 24 hour clock")
-startTime = input("Please enter the time the meeting will start: ")
-endTime = input("Please enter the time the meeting will end: ")
-
-
-if endTime < startTime:
-    print("You have incorrectly inputted your booking times, ensure that the start date is before the end date.")
-else:
-    None
-
+temp = (monthsOfYear[bookingMonth])
+bookingMonth = temp
 
 #Retrieve current date and time
     
-bookingDate = (bookingDay + bookingMonth + bookingYear)
+
 today = date.today()
 
 presentTime = strftime("%H%M", gmtime())
@@ -61,8 +45,45 @@ presentDay = float(today.strftime('%d'))
 presentMonth = float(today.strftime('%m'))
 presentYear = float(today.strftime('%Y'))
 
-startTime2 = startTime.replace(':', '')
-endTime2 = endTime.replace(':', '')
+if (bookingDay < presentDay):
+    ("The date you have inputted has already occured, please try again.")
+    
+elif (bookingMonth < presentMonth):
+    ("The date you have inputted has already occured, please try again.")
+    
+elif (bookingYear < presentYear):
+    ("The date you have inputted has already occured, please try again.")
+
+
+print("Now you will enter the booking time, please use the following format\nExample: 00:00 (Using 12 hour clock) ")
+
+
+userStartTime = input("Please enter the time the meeting will start: ")
+userEndTime = input("Please enter the time the meeting will end: ")
+
+clock1 = int(userStartTime[0:1])
+clock2 = int(userEndTime[0:1])
+
+if clock1 > 12:
+    (clock1)-12
+
+elif clock2 > 12:
+    (clock2)-12
+
+else:
+    None
+
+minutes1 = int(userStartTime[-1:-2])
+minutes2 = int(userEndTime[-1:-2])
+
+startTime = (clock1 + minutes1)
+endTime = (clock2 + minutes2)
+
+if endTime < startTime:
+    print("You have incorrectly inputted your booking times, ensure that the start date is before the end date.")
+else:
+    None
+
 
 
 #Check to ensure that the booking date is not in the past
@@ -76,7 +97,7 @@ elif (bookingMonth < presentMonth):
 elif (bookingYear < presentYear):
     ("The date you have inputted has already occured, please try again.")
     
-elif (startTime2 < presentTime):
+elif (startTime < presentTime):
     ("This time slot has already passed.")
     
 else:
