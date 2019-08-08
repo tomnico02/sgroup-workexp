4ec0576757c4e9d3fcc0ada6df552a13470c7de1
tomnico02 <53854225+tomnico02@users.noreply.github.com>
2019-08-08 15:56:22 +0100

    Meeting room
    Last updated: Thursday 4pm

--- meetingRoom.py
+++ meetingRoom.py
@@ -1,31 +1,31 @@
-
 import datetime
 from datetime import date
 import csv
 
-#All of the users input (name, booking date and times)
+# All of the users input (name, booking date and times)
 name = input("Please enter your name: ")
 
 found = False
 
-f= open('bookingLog.csv', 'r')
+f = open('bookingLog.csv', 'r')
 for line in f:
     if name in line:
         found = True
         bookingYN = input("You already have previous bookings, would you to view them? ")
         if bookingYN == "Yes":
-                with open('bookingLog.csv', 'rt') as f:
-                    reader = csv.reader(f, delimiter=',')
-                    for row in reader:
-                        if name == row[0]:
-                            print("Date booked: " +row[1] +" " +row[2] +" " +row[3] +" Meeting Time: " +row[4] +"-" +row[5])
+            with open('bookingLog.csv', 'rt') as f:
+                reader = csv.reader(f, delimiter=',')
+                for row in reader:
+                    if name == row[0]:
+                        print(
+                            "Date booked: " + row[1] + " " + row[2] + " " + row[3] + " Meeting Time: " + row[4] + "-" +
+                            row[5])
         else:
             None
 
-
 print("You will now need to enter the date you are booking the meeting room for.")
 
-#Retrieve current date and time
+# Retrieve current date and time
 today = date.today()
 dT = datetime.datetime.now()
 presentTime = int(dT.strftime("%H%M"))
@@ -34,13 +34,13 @@ presentDay = float(today.strftime('%d'))
 presentMonth = float(today.strftime('%m'))
 presentYear = float(today.strftime('%Y'))
 
-#Makes sure days of month are capped at 31
+# Makes sure days of month are capped at 31
 bookingDay = int(input("Enter the day you are booking: "))
 while bookingDay >= 32:
     print("Make sure the day is in range 1-31 and is not in the past.")
     bookingDay = int(input("Enter the day you are booking: "))
 
-#Input booking month
+# Input booking month
 userMonth = input("Enter the month you are booking (as a string): ")
 # Stores months of the year as numbers so they can be converted
 monthsOfYear = {
@@ -62,7 +62,7 @@ monthsOfYear = {
 temp = (monthsOfYear[userMonth])
 bookingMonth = temp
 
-#Makes sure the year booked is capped at 2100
+# Makes sure the year booked is capped at 2100
 bookingYear = int(input("Enter the year you are booking: "))
 while bookingYear > 2030:
     print("Make sure the year you inputted is not after 2030 and is not in the past.")
@@ -80,9 +80,67 @@ if endTime < startTime:
 else:
     None
 
+meetingRoom = ['room1', 'room2', 'room3', 'room4']
+emptyRoom = False
+chosenRoom = None
+userRoom = input("Please choose a meeting room by typing 1-4: ")
 
-#Check to ensure that the booking date is not in the past
-#Reading and writing to the text file
+# Makes sure that the users chosen room is not booked already
+while not emptyRoom:
+    if userRoom == "1":
+        csv_file = csv.reader(open('bookingLog.csv', "r"), delimiter=",")
+        if "room1" in csv_file:
+            print("That meeting room is booked, please choose another.")
+            empty = False
+
+        else:
+            empty = True
+            chosenRoom = meetingRoom[0]
+            break
+
+    elif userRoom == "2":
+        csv_file = csv.reader(open('bookingLog.csv', "r"), delimiter=",")
+        if "room2" in csv_file:
+            print("That meeting room is booked, please choose another.")
+            empty = False
+
+        else:
+            empty = True
+            chosenRoom = meetingRoom[1]
+            break
+
+    elif userRoom == "3":
+        csv_file = csv.reader(open('bookingLog.csv', "r"), delimiter=",")
+        if "room3" in csv_file:
+            print("That meeting room is booked, please choose another.")
+            empty = False
+
+        else:
+            empty = True
+            chosenRoom = meetingRoom[2]
+            break
+
+    elif userRoom == "4":
+        csv_file = csv.reader(open('bookingLog.csv', "r"), delimiter=",")
+        if "room4" in csv_file:
+            print("That meeting room is booked, please choose another.")
+            empty = False
+
+        else:
+            empty = True
+            chosenRoom = meetingRoom[3]
+            break
+
+else:
+    print("All of the rooms are booked out at that time, please choose another time.")
+    print("You can use both 12 or 24 hour times but use '0930' format.")
+    startTime = int(input("Please enter the time the meeting will start: "))
+    endTime = int(input("Please enter the time the meeting will end: "))
+    chosenRoom = None
+    emptyRoom = False
+
+# Check to ensure that the booking date is not in the past
+# Reading and writing to the text file
 strDay = str(bookingDay)
 strYear = str(bookingYear)
 
@@ -95,6 +153,7 @@ if len(time1) == 3:
     time1 = temp2
 else:
     temp = time1[:2] + ':' + time1[2:]
+    time1 = temp
 
 if len(time2) == 3:
     temp1 = '0' + time2
@@ -102,14 +161,16 @@ if len(time2) == 3:
     time2 = temp2
 else:
     temp = time2[:2] + ':' + time2[2:]
+    time2 = temp
 
-booking = [name, strDay, userMonth, strYear, time1, time2]
+booking = [name, strDay, userMonth, strYear, time1, time2, chosenRoom]
 
 with open('bookingLog.csv', mode='a') as csv_file:
-    fieldnames = ['Name', 'Day', 'Month', 'Year', 'Meeting Starts', 'Meeting Ends']
+    fieldnames = ['Name', 'Day', 'Month', 'Year', 'Meeting Starts', 'Meeting Ends', 'Chosen Room']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 with open("bookingLog.csv", "a") as f:
-     for item in booking:
-         f.write("%s," % item)
-     f.write("\n")
+    for item in booking:
+        f.write("%s, " % item)
+    f.write("\n")
 
+print("Your meeting has successfully been booked.")
