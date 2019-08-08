960a8c34b7308fca126c6e61f1a9f3609091938b
tomnico02 <53854225+tomnico02@users.noreply.github.com>
2019-08-08 12:03:32 +0100

    Backup - 12:03pm

--- meetingRoom.py
+++ meetingRoom.py
@@ -1,83 +1,91 @@
-import datetime
-import time
-from datetime import date
-from datetime import time
-from time import gmtime, strftime
 
+import datetime
+from datetime import date
+import csv
 
 #All of the users input (name, booking date and times)
-
 name = input("Please enter your name: ")
+
+found = False
+
+f= open('bookingLog.csv', 'r')
+for line in f:
+    if name in line:
+        found = True
+        bookingYN = input("You already have previous bookings, would you to view them? ")
+        if bookingYN == "Yes":
+            with open("bookingLog.csv", 'r') as inp:
+                row = csv.reader(inp)
+        for i in row:
+            if name in i[0]:
+                print(i[1:5])
+
+
 print("You will now need to enter the date you are booking the meeting room for.")
 
-bookingDay = int(input("Enter the day you are booking: "))
-bookingMonth = input("Enter the month you are booking: ")
-bookingYear = int(input("Enter the year you are booking: "))
-
-
-monthsOfYear = {
-    
-    "January":1,
-    "February":2,
-    "March":3,
-    "April":4,
-    "May":5,
-    "June":6,
-    "July":7,
-    "August":8,
-    "September":9,
-    "October":10,
-    "November":11,
-    "December":12
-}
-
-
-temp = (monthsOfYear[bookingMonth])
-bookingMonth = temp
-
 #Retrieve current date and time
-    
-
 today = date.today()
+dT = datetime.datetime.now()
+presentTime = int(dT.strftime("%H%M"))
 
-presentTime = strftime("%H%M", gmtime())
 presentDay = float(today.strftime('%d'))
 presentMonth = float(today.strftime('%m'))
 presentYear = float(today.strftime('%Y'))
 
+
+#Makes sure days of month are capped at 31
+bookingDay = int(input("Enter the day you are booking: "))
+while (bookingDay >= 32 or bookingDay < presentDay):
+    print("Make sure the day is in range 1-31 and is not in the past.")
+    bookingDay = int(input("Enter the day you are booking: "))
+
+#Input booking month
+userMonth = input("Enter the month you are booking (as a string): ")
+# Stores months of the year as numbers so they can be converted
+monthsOfYear = {
+
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
+}
+
+temp = (monthsOfYear[userMonth])
+bookingMonth = temp
+
+while (bookingMonth >12 or bookingMonth > presentMonth):
+    print("Make sure the month you entered is correct and is not in the past.")
+    bookingMonth = int(input("Enter the day month are booking: "))
+
+#Makes sure the year booked is capped at 2100
+bookingYear = int(input("Enter the year you are booking: "))
+while (bookingYear > 2100 or bookingYear < presentYear):
+    print("Make sure the year you inputted is not after 2100 and is not in the past.")
+    bookingDay = int(input("Enter the year you are booking: "))
+
+
 if (bookingDay < presentDay):
     ("The date you have inputted has already occured, please try again.")
-    
 elif (bookingMonth < presentMonth):
     ("The date you have inputted has already occured, please try again.")
-    
 elif (bookingYear < presentYear):
     ("The date you have inputted has already occured, please try again.")
 
 
-print("Now you will enter the booking time, please use the following format\nExample: 00:00 (Using 12 hour clock) ")
 
+bookingDate = (bookingDay + bookingMonth + bookingYear)
 
-userStartTime = input("Please enter the time the meeting will start: ")
-userEndTime = input("Please enter the time the meeting will end: ")
-
-clock1 = int(userStartTime[0:1])
-clock2 = int(userEndTime[0:1])
-
-if clock1 > 12:
-    (clock1)-12
-
-elif clock2 > 12:
-    (clock2)-12
-
-else:
-    None
-
-minutes1 = int(userStartTime[-1:-2])
-minutes2 = int(userEndTime[-1:-2])
-
-startTime = (clock1 + minutes1)
-endTime = (clock2 + minutes2)
+print("Now you will enter the booking time. \nYou can use both 12 or 24 hour times but use '0930' format.")
+startTime = int(input("Please enter the time the meeting will start: "))
+endTime = int(input("Please enter the time the meeting will end: "))
 
 if endTime < startTime:
     print("You have incorrectly inputted your booking times, ensure that the start date is before the end date.")
@@ -85,43 +93,44 @@ else:
     None
 
 
-
 #Check to ensure that the booking date is not in the past
-
-if (bookingDay < presentDay):
-    ("The date you have inputted has already occured, please try again.")
-    
-elif (bookingMonth < presentMonth):
-    ("The date you have inputted has already occured, please try again.")
-    
-elif (bookingYear < presentYear):
-    ("The date you have inputted has already occured, please try again.")
-    
-elif (startTime < presentTime):
-    ("This time slot has already passed.")
-    
-else:
-    print("The date and time you have selected is available, the meeting room has been booked for you")
-
-
 #Reading and writing to the text file
+strDay = str(bookingDay)
+strYear = str(bookingYear)
+strTime1 = str(startTime)
+strTime2 = str(endTime)
 
-f= open("bookings.txt","w+")
+if (startTime < presentTime):
+    ("This time slot has already passed.")
 
-f.write(name)
-f.write(bookingDate)
-f.write(startTime)
-f.write(endTime)
-f.close()
+elif (bookingDay < presentDay):
+    ("Error. You are trying to book a date in the past.")
+
+elif (userMonth < bookingMonth):
+    ("Error. You are trying to book a date in the past.")
+
+elif (bookingYear < presentYear):
+    ("Error. You are trying to book a date in the past.")
+
+else:
+    booking = [name, strDay, userMonth, strYear, strTime1, strTime2]
+
+    with open('bookingLog.csv', mode='a') as csv_file:
+        fieldnames = ['Name', 'Day', 'Month', 'Year', 'Meeting Starts', 'Meeting Ends']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+
+    with open("bookingLog.csv", "a") as f:
+        for item in booking:
+            f.write("%s," % item)
 
 
-
-
-
-
-
-
-
-
-
+#while (emptySlot != True):
+ #   break
+  #  if bookingDate:
+   #     emptySlot = True
+    #elif startTime:
+     #   emptySlot = True
+    #elif endTime:
+        #emptySlot = True
 
